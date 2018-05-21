5a77f77a7e45ac7e5ab54cae955227eaf46eb313
Richard <jiangyijun@huolicai.com>
2018-05-21 17:06:51 +0800

    no message

--- system/shopwap/class/mobile/outchargegold.php
+++ system/shopwap/class/mobile/outchargegold.php
@@ -17,9 +17,9 @@ if (empty($member["outgoldinfo"])) {
 
 $op = $_GP['op'] ? $_GP['op'] : 'display';
 if ($op == 'display') {
-
+    echo 0;
     if (checksubmit('submit')) {
-
+    echo 1;
         if (empty($_GP['charge']) || round($_GP['charge'], 2) <= 0) {
             message("请输入要提取的金额");
         }
